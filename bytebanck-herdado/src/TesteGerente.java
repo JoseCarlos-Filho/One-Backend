8e825a4427a6cc2ec7e432ae23b9030fbba2313f
José Carlos <etijcvivi@gmail.com>
2023-02-20 22:51:13 -0300

    feat: Projeto utilizando herança entre classes.

---
+++ bytebanck-herdado/src/TesteGerente.java
@@ -0,0 +1,22 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+
+		Gerente g1 = new Gerente();
+		
+		g1.setNome("Joao");
+		g1.setCpf("123456789-01");
+		g1.setSalario(5000.0);
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(1111);
+		boolean autenticou = g1.autentica(1111);
+		
+		System.out.println(autenticou);
+	}
+
+}
