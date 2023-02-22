031d846c0a59b3bc569fb519bac55cbfcc9cedfa
José Carlos <etijcvivi@gmail.com>
2023-02-21 22:55:35 -0300

    feat: criando a classe ControleBonificacao().

---
+++ bytebanck-herdado/src/ControleBonificacao.java
@@ -0,0 +1,14 @@
+
+public class ControleBonificacao {
+	
+		private double soma;
+		
+		public void registra(Funcionario f) {
+			double bonifica = f.getBonificacao();
+			this.soma = this.soma + bonifica;
+		}
+		
+		public double getSoma() {
+			return soma;
+		}
+}
