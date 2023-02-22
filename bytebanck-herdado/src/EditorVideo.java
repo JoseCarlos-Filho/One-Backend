4f388c3f5d3c5bf6b7083e873da6e40e418e7b2b
José Carlos <etijcvivi@gmail.com>
2023-02-21 22:58:14 -0300

    feat: Criando a classe EditorVideo().

---
+++ bytebanck-herdado/src/EditorVideo.java
@@ -0,0 +1,10 @@
+// Gerente é um funcionário, gerente herda da classe Funcionario
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		// super referencia o atributo na classe mãe/pai
+		// classe gerente é filho da classe funcionário
+		System.out.println("Metodo: Bonificacao do Editor de Video");
+		return super.getBonificacao() + 100;  
+	}
+}
