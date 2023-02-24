47fcb3682601bbe1d56e2835a92144374a814f60
José Carlos <etijcvivi@gmail.com>
2023-02-23 22:33:54 -0300

    feat: alterando as referencia do objeto funcionario para referencia de objetos concreto(Gerente, EditorVideo, Designer)

--- bytebanck-herdado/src/TesteFuncionário.java
+++ bytebanck-herdado/src/TesteFuncionário.java
@@ -3,7 +3,7 @@ public class TesteFuncionário {
 
 	public static void main(String[] args) {
 		
-		Funcionario paulo = new Funcionario();
+		Gerente paulo = new Gerente();
 		paulo.setNome("Paulo");
 		paulo.setCpf("111.333.888-99");
 		paulo.setSalario(2600.00);
