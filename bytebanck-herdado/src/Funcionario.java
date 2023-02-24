b686c7fa8ea7096c7bd7ce8a2145e75ad488ec38
José Carlos <etijcvivi@gmail.com>
2023-02-23 22:29:12 -0300

    feat: tornando a classe Funcionario em uma classe abstrata.

--- bytebanck-herdado/src/Funcionario.java
+++ bytebanck-herdado/src/Funcionario.java
@@ -1,12 +1,12 @@
 
-public class Funcionario {
+//classes abstrata não pode ser instanciada diretamente, somente os filhos dessa classe.
+public abstract class Funcionario {
 	private String nome;
 	private String cpf;
 	private double salario; // protected atributos do pai pode ser vistos somente pelas classes filhos.
 	
-	public double getBonificacao() {
-		return this.salario * 0.05;
-	}
+	// metodo sem corpo, não há implementação
+	public abstract double getBonificacao();
 	
 	public String getNome() {
 		return nome;
