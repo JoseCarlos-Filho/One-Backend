091b29cb2a9c4fdd8d8a8d14d9f12dfee0dea018
José Carlos <etijcvivi@gmail.com>
2023-02-24 21:54:19 -0300

    fix: removendo setSenha e autentica da classe Gerente.

--- bytebanck-herdado/src/Gerente.java
+++ bytebanck-herdado/src/Gerente.java
@@ -1,18 +1,5 @@
 // Gerente é um funcionário, gerente herda da classe Funcionario
 public class Gerente extends Funcionario {
-	private int senha;
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
 	
 	public double getBonificacao() {
 		// super referencia o atributo na classe mãe/pai
