66a940830c5c8b775565ee07d620b642f4c35d72
José Carlos <etijcvivi@gmail.com>
2023-02-25 22:39:56 -0300

    feat: instancia do objeto cliente com polimorfismo da classe Autenticavel.

--- bytebanck-herdado/src/TesteGerente.java
+++ bytebanck-herdado/src/TesteGerente.java
@@ -4,6 +4,7 @@ public class TesteGerente {
 	public static void main(String[] args) {
 
 		Gerente g1 = new Gerente();
+		Autenticavel referencia = new Gerente();
 		
 		g1.setNome("Joao");
 		g1.setCpf("123456789-01");
