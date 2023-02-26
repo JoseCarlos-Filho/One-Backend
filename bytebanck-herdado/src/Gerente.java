0b5e46bf7f5464db257b253231e86a07c92498c0
José Carlos <etijcvivi@gmail.com>
2023-02-25 22:37:33 -0300

    feat: utilizando implements Autenticavel na classe Gerente e utilizando composição para reutilização de código.

--- bytebanck-herdado/src/Gerente.java
+++ bytebanck-herdado/src/Gerente.java
@@ -1,5 +1,11 @@
-// Gerente é um funcionário, gerente herda da classe Funcionario
-public class Gerente extends Funcionario {
+// Gerente é um funcionário, gerente herda da classe Funcionario, assina a interface Autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
+	
+	private ValidaAutenticacao validador;
+	
+	public Gerente() {
+		this.validador = new ValidaAutenticacao();
+	}
 	
 	public double getBonificacao() {
 		// super referencia o atributo na classe mãe/pai
@@ -8,5 +14,17 @@ public class Gerente extends Funcionario {
 		return super.getSalario();
 		//return super.getBonificacao();
 		//return super.getBonificacao() + super.getSalario();  
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.validador.setSenha(senha);
+		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		//retorna um boleano
+		return this.validador.autentica(senha);
 	}
 }
