2b213be0bb87fd3ca898d0ddeca10b3850f1317c
José Carlos <etijcvivi@gmail.com>
2023-02-26 22:17:49 -0300

    feat: interface Tributavel com meto abstrato getValorImposto();

---
+++ bytebanck-herdado-conta/src/Tributavel.java
@@ -0,0 +1,10 @@
+
+public interface Tributavel {
+	
+	// public abstract é o patrão para metodos de uma interface
+	// portanto não há necessidade de declara-los por que o compilador java já reconhece
+	// este tipo de parâmetro.
+	double getValorImposto();
+
+		
+}
