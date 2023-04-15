bb6fd3d18ac4bd66873dfe6a8b976e888cffc974
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-14 21:11:56 -0300

    feat: classe que testa conexão com banco de dados mysql loja_virtual

---
+++ loja-virtual-repository/src/TestaConexao.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
+		System.out.println(con);
+		System.out.println("Fechando conexao!");
+		con.close();
+
+	}
+
+}
