1540e0b870f1f69661437d800cf4f4be635fa45b
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-18 21:48:31 -0300

    feat: classe que efetua a listagem de categorias e produtos relacionando tabelas.

---
+++ loja-virtual-repository/src/TestaListagemDeCategoria.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.CategoriaDAO;
+import dao.ProdutoDAO;
+import factory.ConnectionFactory;
+import modelo.Categoria;
+import modelo.Produto;
+
+public class TestaListagemDeCategoria {
+
+	public static void main(String[] args) throws SQLException {
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+			List<Categoria> ListaDeCategorias = categoriaDAO.listarComProdutos();
+			ListaDeCategorias.stream().forEach(lct -> {
+				System.out.println(lct.getNome());
+//				try {
+//					for(Produto produto: new ProdutoDAO(connection).buscar(lct)) {
+//						System.out.println(lct.getNome() + " - " + produto.getNome());
+//					}
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+			});
+		}
+	}
+
+}
