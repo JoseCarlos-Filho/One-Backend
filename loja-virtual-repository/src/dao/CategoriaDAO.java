80aebeefc98d5dd43b5d6c8d499e0279cdf11688
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-18 21:54:57 -0300

    feat: Data Access Object da tabela categoria

---
+++ loja-virtual-repository/src/dao/CategoriaDAO.java
@@ -0,0 +1,56 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Categoria;
+
+public class CategoriaDAO {
+
+	private Connection connection;
+
+	public CategoriaDAO(Connection connection) {
+		this.connection = connection;
+	}
+
+	public List<Categoria> listar() throws SQLException {
+		List<Categoria> categorias = new ArrayList<>();
+
+		String sql = "SELECT ID, NOME FROM CATEGORIA";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getResultSet()) {
+				while (rst.next()) {
+					Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+					categorias.add(categoria);
+				}
+			}
+		}
+		return categorias;
+	}
+
+	public List<Categoria> listarComProdutos() throws SQLException {
+		List<Categoria> categorias = new ArrayList<>();
+
+		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN"
+				+ " PRODUTO P ON C.ID = P.CATEGORIA_ID";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getResultSet()) {
+				while (rst.next()) {
+					Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+					categorias.add(categoria);
+				}
+			}
+		}
+		return categorias;
+	}
+}
