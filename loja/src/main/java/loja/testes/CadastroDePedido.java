4cf45077e4807a0bedba60a91ef956352784dbd7
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-25 22:43:51 -0300

    feat: classe de cadastro de pedidos

---
+++ loja/src/main/java/loja/testes/CadastroDePedido.java
@@ -0,0 +1,65 @@
+package loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import loja.dao.CategoriaDAO;
+import loja.dao.ClienteDAO;
+import loja.dao.PedidoDAO;
+import loja.dao.ProdutoDAO;
+import loja.modelo.Categoria;
+import loja.modelo.Cliente;
+import loja.modelo.ItemPedido;
+import loja.modelo.Pedido;
+import loja.modelo.Produto;
+import loja.util.JPAUtil;
+
+public class CadastroDePedido {
+
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		ClienteDAO clienteDao = new ClienteDAO(em);
+		
+		Produto produto = produtoDao.buscarPorId(1l);
+		Cliente cliente = clienteDao.buscarPorId(1l);
+		
+		em.getTransaction().begin();
+		
+		
+		Pedido pedido = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		
+		PedidoDAO pedidoDao = new PedidoDAO(em);
+		pedidoDao.cadastrar(pedido);
+		
+		em.getTransaction().commit();
+		
+		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
+		System.out.println("Valor total : " + totalVendido);
+	}
+	
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Samsung", "Galaxy s22", new BigDecimal("7000"), celulares);
+		
+		Cliente cliente = new Cliente("José", "30012345687");
+		
+		EntityManager em = JPAUtil.getEntityManager();
+
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
+		ClienteDAO clienteDao = new ClienteDAO(em);
+
+		em.getTransaction().begin();
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		clienteDao.cadastrar(cliente);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
