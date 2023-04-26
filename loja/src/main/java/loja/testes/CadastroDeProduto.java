06da63dc544c220cfec01016ccdedb31b4937606
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-25 22:44:20 -0300

    feat: classe cadastro de produtos

---
+++ loja/src/main/java/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,47 @@
+package loja.testes;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import loja.dao.CategoriaDAO;
+import loja.dao.ProdutoDAO;
+import loja.modelo.Categoria;
+import loja.modelo.Produto;
+import loja.util.JPAUtil;
+
+public class CadastroDeProduto {
+
+	public static void main(String[] args) {
+
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+		todos.forEach(td -> System.out.println(td.getNome()));
+		
+		BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Samsung");
+		System.out.println("Preço do produto : " + precoDoProduto);
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Samsung", "Galaxy s22", new BigDecimal("7000"), celulares);
+
+		EntityManager em = JPAUtil.getEntityManager();
+
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
+
+		em.getTransaction().begin();
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
