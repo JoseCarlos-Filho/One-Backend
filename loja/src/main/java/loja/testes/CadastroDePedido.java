63ff8bc16954daeaa89be786f975a8fa8b8268f3
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-26 22:44:19 -0300

    feat: adicionando pedido de produto e criando uma classe Vo:Value Object

--- loja/src/main/java/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/loja/testes/CadastroDePedido.java
@@ -1,6 +1,7 @@
 package loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -14,6 +15,7 @@ import loja.modelo.ItemPedido;
 import loja.modelo.Pedido;
 import loja.modelo.Produto;
 import loja.util.JPAUtil;
+import loja.vo.RelatorioDeVendasVo;
 
 public class CadastroDePedido {
 
@@ -25,6 +27,8 @@ public class CadastroDePedido {
 		ClienteDAO clienteDao = new ClienteDAO(em);
 		
 		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
 		Cliente cliente = clienteDao.buscarPorId(1l);
 		
 		em.getTransaction().begin();
@@ -32,19 +36,32 @@ public class CadastroDePedido {
 		
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+		
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido, produto3));		
 		
 		PedidoDAO pedidoDao = new PedidoDAO(em);
 		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
 		
 		em.getTransaction().commit();
 		
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("Valor total : " + totalVendido);
+		
+		List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+		relatorio.forEach(System.out::println);
 	}
 	
 	private static void popularBancoDeDados() {
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Samsung", "Galaxy s22", new BigDecimal("7000"), celulares);
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
+		Produto celular = new Produto("Samsung", "Galaxy s22", new BigDecimal("6000"), celulares);
+		Produto videogame = new Produto("PS5", "PlayStation", new BigDecimal("2500"), videogames);
+		Produto macbook = new Produto("Macbook", "Macbook pro", new BigDecimal("9000"), informatica);
 		
 		Cliente cliente = new Cliente("José", "30012345687");
 		
@@ -55,8 +72,15 @@ public class CadastroDePedido {
 		ClienteDAO clienteDao = new ClienteDAO(em);
 
 		em.getTransaction().begin();
+		
 		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
 		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
 		clienteDao.cadastrar(cliente);
 		em.getTransaction().commit();
 		em.close();
